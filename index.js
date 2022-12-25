7ea722d9c4797e5dd81adbceb5b4d84323512bb9
Shashank Tripathi <shashanktripathi.st15a@gmail.com>
2022-12-26 02:20:15 +0530

    1

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var cors = require('cors')
 
 
 ConnectToDatabase();
-const app = express()
-const port = 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(express.json());
